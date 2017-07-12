99d3c2e5ccbdd3d79ae15bae42ad1bedf4261154
TSC_DMS <787753577@qq.com>
2017-07-12 17:22:12 +0800

    添加装饰模式

---
+++ 3、装饰模式/3、装饰模式/Source/Component.swift
@@ -0,0 +1,13 @@
+//
+//  Component.swift
+//  3、装饰模式
+//
+//  Created by tsc on 2017/7/12.
+//  Copyright © 2017年 tsc. All rights reserved.
+//
+
+import UIKit
+
+class Component: NSObject {
+
+}
---
+++ 3、装饰模式/3、装饰模式/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  3、装饰模式
+//
+//  Created by tsc on 2017/7/12.
+//  Copyright © 2017年 tsc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
