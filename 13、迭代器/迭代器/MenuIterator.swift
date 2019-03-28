8f5279b7d27b0a9e9df22a99d38d4c3e86345cba
Barry <787753577@qq.com>
2019-03-28 18:25:01 +0800

    迭代器

---
+++ 13、迭代器/迭代器/MenuIterator.swift
@@ -0,0 +1,13 @@
+//
+//  MenuIterator.swift
+//  迭代器
+//
+//  Created by Honey on 2019/3/28.
+//  Copyright © 2019 Honey. All rights reserved.
+//
+
+import Foundation
+
+protocol MenuIterator {
+    func createIterator() -> Iterator
+}
